7f9f9d2c5973a4464e4421e279e9b7dad22765ba
ynanangaa <nicolas.ananga78@gmail.com>
2025-01-14 20:46:18 +0100

    Update __init__.py files

--- california_houseprice_prediction/domain/__init__.py
+++ california_houseprice_prediction/domain/__init__.py
@@ -3,3 +3,9 @@ from .train_random_forest_model import train_and_log_random_forest_model
 from .train_gradient_boosting_model import (
     train_and_log_gradient_boosting_model,
 )
+
+__all__ = [
+    "train_and_log_base_model",
+    "train_and_log_random_forest_model",
+    "train_and_log_gradient_boosting_model",
+]
--- california_houseprice_prediction/infrastructure/__init__.py
+++ california_houseprice_prediction/infrastructure/__init__.py
@@ -1,1 +1,3 @@
 from .split_data_train_test import load_and_split_data
+
+__all__ = ["load_and_split_data"]
