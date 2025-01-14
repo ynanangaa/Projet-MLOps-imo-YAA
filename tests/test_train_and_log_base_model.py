81e8927347db8f166fe12753caf1481da3a58e2d
ynanangaa <nicolas.ananga78@gmail.com>
2025-01-14 17:00:36 +0100

    Update test_train_and_log_base_model.py

--- tests/test_train_and_log_base_model.py
+++ tests/test_train_and_log_base_model.py
@@ -9,4 +9,4 @@ def test_train_and_log_base_model():
     X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
     
     # Tester que la fonction s'exécute sans erreur
-    train_and_log_model(X_train, X_test, y_train, y_test)
+    train_and_log_base_model(X_train, X_test, y_train, y_test)
